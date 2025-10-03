dced0275dad914be0428ea282ef24e47c93848e9
admingroup2025 <info@admiregroup.org>
2025-10-03 09:10:04 +0530

    test-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import Home from './component/pages/Home'
+import Home from './component/pages/Home.jsx'
 
 
 function App() {
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import CreateRoom from "./component/pages/CreateRoom.jsx";
-import Game from "./component/gamePage/game.jsx";
+import Game from "./component/gamePage/Game.jsx";
 import Room from "./component/pages/Room.jsx";
 
 createRoot(document.getElementById('root')).render(
